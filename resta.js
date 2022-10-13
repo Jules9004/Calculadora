e11211554bbe8efb8b3e374f1e4df67d0ee7948e
Jules9004 <julytroldan@yahoo.com>
2022-10-12 23:36:16 -0500

    Calc

---
+++ resta.js
@@ -0,0 +1,5 @@
+function sub(num1, num2) {
+    let oper = num1 - num2;
+    return oper;
+}
+export {sub}
---
+++ suma.js
@@ -0,0 +1,5 @@
+function sum(numero1, numero2){
+    let oper =numero1+ numero2
+    return oper
+}
+export{sum}
